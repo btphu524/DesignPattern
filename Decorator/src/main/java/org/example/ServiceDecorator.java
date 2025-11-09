5eaee25969d43199a87f802b675958ce27146399
ThanhPhu <buithanhphu05022004@gmail.com>
2025-11-09 22:44:33 +0700

    decorator

---
+++ Decorator/src/main/java/org/example/ServiceDecorator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public abstract class ServiceDecorator implements Service {
+    protected Service service;
+
+    public ServiceDecorator(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public String getDescription() {
+        return service.getDescription();
+    }
+    @Override
+    public double getCost() {
+        return service.getCost();
+    }
+}
